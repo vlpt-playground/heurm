1d80ad9efa6ec39247efbc3088ff075dee1526b8
velopert <public.velopert@gmail.com>
2017-07-01 04:48:33 +0900

    미들웨어 테스팅

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,6 +1,21 @@
 const Koa = require('koa');
 const app = new Koa();
 
+app.use(async (ctx, next) => {
+    console.log(1);
+    const started = new Date();
+    await next();
+    console.log(new Date() - started + 'ms');
+});
+
+
+app.use((ctx, next) => {
+    console.log(2);
+    next();
+});
+
+
+
 app.use(ctx => {
     ctx.body = 'Hello Koa';
 });
