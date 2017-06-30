fb9431d57943dea85f38cdfbf79000f27a45a6ed
velopert <public.velopert@gmail.com>
2017-07-01 05:23:45 +0900

    데이터 생성

--- heurm-server/src/api/books/books.controller.js
+++ heurm-server/src/api/books/books.controller.js
@@ -1,9 +1,43 @@
+const Book = require('models/book');
+
 exports.list = (ctx) => {
     ctx.body = 'listed';
 };
 
-exports.create = (ctx) => {
-    ctx.body = 'created';
+exports.create = async (ctx) => {
+    // request body 에서 값들을 추출합니다
+    const { 
+        title, 
+        authors, 
+        publishedDate, 
+        price, 
+        tags 
+    } = ctx.request.body;
+
+    // Book 인스턴스를 생성합니다
+    const book = new Book({
+        title, 
+        authors,
+        publishedDate,
+        price,
+        tags
+    });
+
+    // 만들어진 Book 인스턴스를, 이렇게 수정 할 수도 있습니다.
+    // book.title = title;
+
+    // .save() 함수를 실행하면 이 때 데이터베이스에 실제로 데이터를 작성합니다.
+    // Promise 를 반환합니다.
+    try {
+        await book.save();
+    } catch(e) {
+        // HTTP 상태 500 와 Internal Error 라는 메시지를 반환하고, 
+        // 에러를 기록합니다.
+        return ctx.throw(500, e);
+    }
+
+    // 저장한 결과를 반환합니다.
+    ctx.body = book;
 };
 
 exports.delete = (ctx) => {
