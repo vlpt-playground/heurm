05ff24d6dd385aa6cce170bed0382c6e976de231
velopert <public.velopert@gmail.com>
2017-07-01 06:18:27 +0900

    회원가입 페이지

--- heurm-client/src/containers/Auth/Register.js
+++ heurm-client/src/containers/Auth/Register.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
-import { AuthContent } from 'components/Auth';
+import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from 'components/Auth';
+
 
 class Register extends Component {
     render() {
         return (
             <AuthContent title="회원가입">
-
+                <InputWithLabel label="이메일" name="email" placeholder="이메일"/>
+                <InputWithLabel label="아이디" name="username" placeholder="아이디"/>
+                <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
+                <InputWithLabel label="비밀번호 확인" name="passwordConfirm" placeholder="비밀번호 확인" type="password"/>
+                <AuthButton>회원가입</AuthButton>
+                <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
             </AuthContent>
         );
     }
