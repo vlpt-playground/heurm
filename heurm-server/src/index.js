fee3bf80f81343b8665a81918d673681372434bd
velopert <public.velopert@gmail.com>
2017-07-01 05:21:01 +0900

    bodyParser 적용

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -8,6 +8,7 @@ const router = new Router();
 const api = require('./api');
 
 const mongoose = require('mongoose');
+const bodyParser = require('koa-bodyparser');
 
 mongoose.Promise = global.Promise; // Node 의 네이티브 Promise 사용
 // mongodb 연결
@@ -21,8 +22,9 @@ mongoose.connect(process.env.MONGO_URI).then(
 
 const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
 
-router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
+app.use(bodyParser()); // 바디파서 적용, 라우터 적용코드보다 상단에 있어야합니다.
 
+router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(port, () => {
