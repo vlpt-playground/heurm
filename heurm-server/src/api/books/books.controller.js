b7a4824f01a78889a8fa63a9bc0dedb4f2da6c68
velopert <public.velopert@gmail.com>
2017-07-01 04:55:28 +0900

    여러메소드 사용하기

---
+++ heurm-server/src/api/books/books.controller.js
@@ -0,0 +1,19 @@
+exports.list = (ctx) => {
+    ctx.body = 'listed';
+};
+
+exports.create = (ctx) => {
+    ctx.body = 'created';
+};
+
+exports.delete = (ctx) => {
+    ctx.body = 'deleted';
+};
+
+exports.replace = (ctx) => {
+    ctx.body = 'replaced';
+};
+
+exports.update = (ctx) => {
+    ctx.body = 'updated';
+};
--- heurm-server/src/api/books/index.js
+++ heurm-server/src/api/books/index.js
@@ -1,9 +1,12 @@
 const Router = require('koa-router');
 
 const books = new Router();
+const booksCtrl = require('./books.controller');
 
-books.get('/', (ctx, next) => {
-    ctx.body = 'GET ' + ctx.request.path;
-});
+books.get('/', booksCtrl.list);
+books.post('/', booksCtrl.create);
+books.delete('/', booksCtrl.delete);
+books.put('/', booksCtrl.replace);
+books.patch('/', booksCtrl.update);
 
 module.exports = books;
