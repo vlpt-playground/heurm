3b2b794a2debdc839abd3cd751534fad2e9700a9
velopert <public.velopert@gmail.com>
2017-07-01 05:30:21 +0900

    계정 컬렉션 스키마 디자인

---
+++ heurm-server/src/models/account.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const Account = new Schema({
+    profile: {
+        username: String,
+        thumbnail: { type: String, default: '/static/images/default_thumbnail.png' }
+    },
+    email: { type: String },
+    // 소셜 계정으로 회원가입을 할 경우에는 각 서비스에서 제공되는 id 와 accessToken 을 저장합니다
+    social: {
+        facebook: {
+            id: String,
+            accessToken: String
+        },
+        google: {
+            id: String,
+            accessToken: String
+        }
+    },
+    password: String, // 로컬계정의 경우엔 비밀번호를 해싱해서 저장합니다
+    thoughtCount: { type: Number, default: 0 }, // 서비스에서 포스트를 작성 할 때마다 1씩 올라갑니다
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Account', Account);
