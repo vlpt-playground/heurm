27b0abf15a907d8af5ede109571d179f2bb863f4
velopert <public.velopert@gmail.com>
2017-07-01 06:20:32 +0900

    Register 인풋상태 연동

--- heurm-client/src/containers/Auth/Register.js
+++ heurm-client/src/containers/Auth/Register.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
 import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from 'components/Auth';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as authActions from 'redux/modules/auth';
 
 
 class Register extends Component {
+
+    handleChange = (e) => {
+        const { AuthActions } = this.props;
+        const { name, value } = e.target;
+
+        AuthActions.changeInput({
+            name,
+            value,
+            form: 'register'
+        });
+    }
+
     render() {
+        const { email, username, password, passwordConfirm } = this.props.form.toJS();
+        const { handleChange } = this;
+
         return (
             <AuthContent title="회원가입">
-                <InputWithLabel label="이메일" name="email" placeholder="이메일"/>
-                <InputWithLabel label="아이디" name="username" placeholder="아이디"/>
-                <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
-                <InputWithLabel label="비밀번호 확인" name="passwordConfirm" placeholder="비밀번호 확인" type="password"/>
+                <InputWithLabel 
+                    label="이메일"
+                    name="email"
+                    placeholder="이메일" 
+                    value={email} 
+                    onChange={handleChange}
+                />
+                <InputWithLabel 
+                    label="아이디" 
+                    name="username" 
+                    placeholder="아이디" 
+                    value={username} 
+                    onChange={handleChange}
+                />
+                <InputWithLabel 
+                    label="비밀번호" 
+                    name="password" 
+                    placeholder="비밀번호"
+                    type="password" 
+                    value={password} 
+                    onChange={handleChange}
+                />
+                <InputWithLabel 
+                    label="비밀번호 확인" 
+                    name="passwordConfirm" 
+                    placeholder="비밀번호 확인" 
+                    type="password" 
+                    value={passwordConfirm}
+                    onChange={handleChange}
+                />
                 <AuthButton>회원가입</AuthButton>
                 <RightAlignedLink to="/auth/login">로그인</RightAlignedLink>
             </AuthContent>
@@ -17,4 +61,11 @@ class Register extends Component {
     }
 }
 
-export default Register;
+export default connect(
+    (state) => ({
+        form: state.auth.getIn(['register', 'form'])
+    }),
+    (dispatch) => ({
+        AuthActions: bindActionCreators(authActions, dispatch)
+    })
+)(Register);
