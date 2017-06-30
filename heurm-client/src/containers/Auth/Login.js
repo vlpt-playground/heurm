bae03ae91b380c16f419c6d21d80b32a3c10a488
velopert <public.velopert@gmail.com>
2017-07-01 07:32:26 +0900

    로그인에서 세션 만료 메시지 띄우기

--- heurm-client/src/containers/Auth/Login.js
+++ heurm-client/src/containers/Auth/Login.js
@@ -5,10 +5,20 @@ import {bindActionCreators} from 'redux';
 import * as authActions from 'redux/modules/auth';
 import * as userActions from 'redux/modules/user';
 import storage from 'lib/storage';
+import queryString from 'query-string';
 
 
 class Login extends Component {
 
+    componentDidMount() {
+        const { location } = this.props;
+        const query = queryString.parse(location.search);
+
+        if(query.expired !== undefined) {
+            this.setError('세션에 만료되었습니다. 다시 로그인하세요.')
+        }
+    }
+    
     handleChange = (e) => {
         const { AuthActions } = this.props;
         const { name, value } = e.target;
