99590acb9c5de04952167a0deb4007303652d3c9
velopert <public.velopert@gmail.com>
2017-07-01 05:31:44 +0900

    hash 함수 생성

--- heurm-server/src/models/account.js
+++ heurm-server/src/models/account.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+const crypto = require('crypto');
+
+function hash(password) {
+    return crypto.createHmac('sha256', process.env.SECRET_KEY).update(password).digest('hex');
+}
 
 const Account = new Schema({
     profile: {
