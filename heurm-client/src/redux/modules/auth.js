4599a8745199a1ecc7e0741680b57e19dee39d8e
velopert <public.velopert@gmail.com>
2017-07-01 06:25:22 +0900

    회원인증 관련 리덕스 액션 준비하기

--- heurm-client/src/redux/modules/auth.js
+++ heurm-client/src/redux/modules/auth.js
@@ -2,11 +2,34 @@ import { createAction, handleActions } from 'redux-actions';
 
 import { Map } from 'immutable';
 
+import { pender } from 'redux-pender';
+import * as AuthAPI from 'lib/api/auth';
+
+
+
 const CHANGE_INPUT = 'auth/CHANGE_INPUT'; // input 값 변경
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM'; // form 초기화
 
+const CHECK_EMAIL_EXISTS = 'auth/CHECK_EMAIL_EXISTS'; // 이메일 중복 확인
+const CHECK_USERNAME_EXISTS = 'auth/CHECK_USERNAME_EXISTS'; // 아이디 중복 확인
+
+const LOCAL_REGISTER = 'auth/LOCAL_REGISTER'; // 이메일 가입
+const LOCAL_LOGIN = 'auth/LOCAL_LOGIN'; // 이메일 로그인
+const LOGOUT = 'auth/LOGOUT'; // 로그아웃
+
+
+
 export const changeInput = createAction(CHANGE_INPUT); //  { form, name, value }
 export const initializeForm = createAction(INITIALIZE_FORM); // form 
+
+export const checkEmailExists = createAction(CHECK_EMAIL_EXISTS, AuthAPI.checkEmailExists); // email
+export const checkUsernameExists = createAction(CHECK_USERNAME_EXISTS, AuthAPI.checkUsernameExists); // username
+
+export const localRegister = createAction(LOCAL_REGISTER, AuthAPI.localRegister); // { email, username, password }
+export const localLogin = createAction(LOCAL_LOGIN, AuthAPI.localLogin); // { email, password }
+export const logout = createAction(LOGOUT, AuthAPI.logout);
+
+
 
 const initialState = Map({
     register: Map({
@@ -15,6 +38,10 @@ const initialState = Map({
             username: '',
             password: '',
             passwordConfirm: ''
+        }),
+        exists: Map({
+            email: false,
+            password: false
         })
     }),
     login: Map({
@@ -22,7 +49,8 @@ const initialState = Map({
             email: '',
             password: ''
         })
-    })
+    }),
+    result: Map({})
 });
 
 export default handleActions({
@@ -33,5 +61,21 @@ export default handleActions({
     [INITIALIZE_FORM]: (state, action) => {
         const initialForm = initialState.get(action.payload);
         return state.set(action.payload, initialForm);
-    }
+    },
+    ...pender({
+        type: CHECK_EMAIL_EXISTS,
+        onSuccess: (state, action) => state.setIn(['register', 'exists', 'email'], action.payload.data.exists)
+    }),
+    ...pender({
+        type: CHECK_USERNAME_EXISTS,
+        onSuccess: (state, action) => state.setIn(['register', 'exists', 'username'], action.payload.data.exists)
+    }),
+    ...pender({
+        type: LOCAL_LOGIN,
+        onSuccess: (state, action) => state.set('result', Map(action.payload.data))
+    }),
+    ...pender({
+        type: LOCAL_REGISTER,
+        onSuccess: (state, action) => state.set('result', Map(action.payload.data))
+    }),
 }, initialState);
