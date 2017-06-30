3515ad37c6812fc9183ae3c5eb708a76a6640a4a
velopert <public.velopert@gmail.com>
2017-07-01 04:50:55 +0900

    koa-router 기본사용

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,24 +1,15 @@
 const Koa = require('koa');
+const Router = require('koa-router');
+
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx, next) => {
-    console.log(1);
-    const started = new Date();
-    await next();
-    console.log(new Date() - started + 'ms');
+router.get('/', (ctx, next) => {
+    ctx.body = '홈';
 });
 
-
-app.use((ctx, next) => {
-    console.log(2);
-    next();
-});
-
-
-
-app.use(ctx => {
-    ctx.body = 'Hello Koa';
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('heurm server is listening to port 4000');
