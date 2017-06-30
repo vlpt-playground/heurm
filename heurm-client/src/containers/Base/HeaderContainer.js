f511b8b9b3e33de10b90d27e3c2ba956ceb7d164
velopert <public.velopert@gmail.com>
2017-07-01 07:31:54 +0900

    임시 로그아웃

--- heurm-client/src/containers/Base/HeaderContainer.js
+++ heurm-client/src/containers/Base/HeaderContainer.js
@@ -1,15 +1,40 @@
 import React, { Component } from 'react';
 import Header, { LoginButton } from 'components/Base/Header';
 import { connect } from 'react-redux';
+import * as userActions from 'redux/modules/user';
+import { bindActionCreators } from 'redux';
+import storage from 'lib/storage';
+
+
 
 class HeaderContainer extends Component {
+
+    handleLogout = async () => {
+        const { UserActions } = this.props;
+        try {
+            await UserActions.logout();
+        } catch (e) {
+            console.log(e);
+        }
+
+        storage.remove('loggedInfo');
+        window.location.href = '/'; // 홈페이지로 새로고침
+    }
+
     render() {
-        const { visible } = this.props;
+        const { visible, user } = this.props;
         if(!visible) return null;
+
+
 
         return (
             <Header>
-                <LoginButton/>
+                { user.get('logged') 
+                    ? (<div>
+                        {user.getIn(['loggedInfo', 'username'])} <div onClick={this.handleLogout}>(로그아웃)</div>
+                    </div> )
+                    : <LoginButton/> 
+                }
             </Header>
         );
     }
@@ -17,9 +42,10 @@ class HeaderContainer extends Component {
 
 export default connect(
     (state) => ({
-        visible: state.base.getIn(['header', 'visible'])
+        visible: state.base.getIn(['header', 'visible']),
+        user: state.user
     }),
     (dispatch) => ({
-
+        UserActions: bindActionCreators(userActions, dispatch)
     })
 )(HeaderContainer);
