6552facce83e00d8a6d8fe20171525100c3449ba
velopert <public.velopert@gmail.com>
2017-07-01 06:23:24 +0900

    redux-pender 적용

--- heurm-client/src/redux/configureStore.js
+++ heurm-client/src/redux/configureStore.js
@@ -1,10 +1,14 @@
-import { createStore } from 'redux';
-
+import { createStore, applyMiddleware, compose } from 'redux';
+import penderMiddleware from 'redux-pender';
 import modules from './modules';
 
-// Todo: 미들웨어 적용
+const isDevelopment = process.env.NODE_ENV === 'development'; // 환경이 개발모드인지 확인합니다
+const composeEnhancers = isDevelopment ? (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose) : compose;
+
 const configureStore = (initialState) => {
-    const store = createStore(modules, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+    const store = createStore(modules, initialState, composeEnhancers(
+        applyMiddleware(penderMiddleware())
+    ));
 
     // hot-reloading 를 위한 코드
     if(module.hot) {
--- heurm-client/src/redux/modules/index.js
+++ heurm-client/src/redux/modules/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import base from './base';
 import auth from './auth';
+import { penderReducer } from 'redux-pender';
 
 export default combineReducers({
     base,
-    auth
+    auth,
+    pender: penderReducer
 });
