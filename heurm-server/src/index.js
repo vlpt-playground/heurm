6d76e8f447a707bb14aa5bdb128114a61872b0d6
velopert <public.velopert@gmail.com>
2017-07-01 04:52:56 +0900

    여러개의 라우트, 라우트 파라미터

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -8,8 +8,25 @@ router.get('/', (ctx, next) => {
     ctx.body = '홈';
 });
 
-app.use(router.routes());
-app.use(router.allowedMethods());
+router.get('/about', (ctx, next) => {
+    ctx.body = '소개';
+});
+
+router.get('/about/:name', (ctx, next) => {
+    const { name } = ctx.params; // 라우트 경로에서 :파라미터명 으로 정의된 값이 ctx.params 안에 설정됩니다.
+    ctx.body = name + '의 소개';
+});
+
+router.get('/post', (ctx, next) => {
+    const { id } = ctx.request.query; // 주소 뒤에 ?id=10 이런식으로 작성된 쿼리는 ctx.request.query 에 파싱됩니다.
+    if(id) {
+        ctx.body = '포스트 #' + id;
+    } else {
+        ctx.body = '포스트 아이디가 없습니다.';
+    }
+});
+
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(4000, () => {
     console.log('heurm server is listening to port 4000');
