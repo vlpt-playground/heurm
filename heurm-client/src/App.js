6c346b07b2784857d549dfae9172ff738c70cb48
velopert <public.velopert@gmail.com>
2017-07-01 07:31:13 +0900

    새로고침 할 시 로그인 유지

--- heurm-client/src/App.js
+++ heurm-client/src/App.js
@@ -3,7 +3,32 @@ import { Route } from 'react-router-dom';
 import { Home, Auth } from 'pages';
 import HeaderContainer from 'containers/Base/HeaderContainer';
 
+import storage from 'lib/storage';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as userActions from 'redux/modules/user';
+
 class App extends Component {
+
+
+    initializeUserInfo = async () => {
+        const loggedInfo = storage.get('loggedInfo'); // 로그인 정보를 로컬스토리지에서 가져옵니다.
+        if(!loggedInfo) return; // 로그인 정보가 없다면 여기서 멈춥니다.
+
+        const { UserActions } = this.props;
+        UserActions.setLoggedInfo(loggedInfo);
+        try {
+            await UserActions.checkStatus();
+        } catch (e) {
+            storage.remove('loggedInfo');
+            window.location.href = '/auth/login?expired';
+        }
+    }
+
+    componentDidMount() {
+        this.initializeUserInfo();
+    }
+
     render() {
         return (
             <div>
@@ -15,4 +40,9 @@ class App extends Component {
     }
 }
 
-export default App;
+export default connect(
+    null,
+    (dispatch) => ({
+        UserActions: bindActionCreators(userActions, dispatch)
+    })
+)(App);
