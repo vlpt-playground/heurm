ba57d1e09d9a66edeb695804e75676954529371f
velopert <public.velopert@gmail.com>
2017-07-01 06:27:54 +0900

    회원가입 인풋 검증 함수 세트 객체 만들기

--- heurm-client/src/containers/Auth/Register.js
+++ heurm-client/src/containers/Auth/Register.js
@@ -3,9 +3,53 @@ import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from 'compo
 import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as authActions from 'redux/modules/auth';
+import {isEmail, isLength, isAlphanumeric} from 'validator';
+
 
 
 class Register extends Component {
+
+setError = (message) => {
+        const { AuthActions } = this.props;
+        AuthActions.setError({
+            form: 'register',
+            message
+        });
+    }
+
+    validate = {
+        email: (value) => {
+            if(!isEmail(value)) {
+                this.setError('잘못된 이메일 형식 입니다.');
+                return false;
+            }
+            return true;
+        },
+        username: (value) => {
+            if(!isAlphanumeric(value) || !isLength(value, { min:4, max: 15 })) {
+                this.setError('아이디는 4~15 글자의 알파벳 혹은 숫자로 이뤄져야 합니다.');
+                return false;
+            }
+            return true;
+        },
+        password: (value) => {
+            if(!isLength(value, { min: 6 })) {
+                this.setError('비밀번호를 6자 이상 입력하세요.');
+                return false;
+            }
+            this.setError(null); // 이메일과 아이디는 에러 null 처리를 중복확인 부분에서 하게 됩니다
+            return true;
+        },
+        passwordConfirm: (value) => {
+            if(this.props.form.get('password') !== value) {
+                this.setError('비밀번호확인이 일치하지 않습니다.');
+                return false;
+            }
+            this.setError(null); 
+            return true;
+        }
+    }
+
 
     handleChange = (e) => {
         const { AuthActions } = this.props;
@@ -16,6 +60,12 @@ class Register extends Component {
             value,
             form: 'register'
         });
+
+        // 검증작업 진행
+        const validation = this.validate[name](value);
+        if(name.indexOf('password') > -1 || !validation) return; // 비밀번호 검증이거나, 검증 실패하면 여기서 마침
+
+        // TODO: 이메일, 아이디 중복 확인
     }
 
     componentWillUnmount() {
