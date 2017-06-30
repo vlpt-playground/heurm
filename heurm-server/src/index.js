cbcb74759742620b699b96c6023087a2323aa1bc
velopert <public.velopert@gmail.com>
2017-07-01 04:57:06 +0900

    .env 사용하기

--- heurm-server/src/index.js
+++ heurm-server/src/index.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); // .env 파일에서 환경변수 불러오기
+
 const Koa = require('koa');
 const Router = require('koa-router');
 
@@ -5,10 +7,12 @@ const app = new Koa();
 const router = new Router();
 const api = require('./api');
 
+const port = process.env.PORT || 4000; // PORT 값이 설정되어있지 않다면 4000 을 사용합니다.
+
 router.use('/api', api.routes()); // api 라우트를 /api 경로 하위 라우트로 설정
 
 app.use(router.routes()).use(router.allowedMethods());
 
-app.listen(4000, () => {
-    console.log('heurm server is listening to port 4000');
+app.listen(port, () => {
+    console.log('heurm server is listening to port ' + port);
 });
