d7ec1acf8a6387e749e4db3277c8b220eba703fc
velopert <public.velopert@gmail.com>
2017-07-01 06:20:11 +0900

    로그인 인풋 상태 연동

--- heurm-client/src/containers/Auth/Login.js
+++ heurm-client/src/containers/Auth/Login.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
 import { AuthContent, InputWithLabel, AuthButton, RightAlignedLink } from 'components/Auth';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as authActions from 'redux/modules/auth';
 
 class Login extends Component {
+
+    handleChange = (e) => {
+        const { AuthActions } = this.props;
+        const { name, value } = e.target;
+
+        AuthActions.changeInput({
+            name,
+            value,
+            form: 'login'
+        });
+    }
+
     render() {
+        const { email, password } = this.props.form.toJS(); // form 에서 email 과 password 값을 읽어옴
+        const { handleChange } = this;
+
         return (
             <AuthContent title="로그인">
-                <InputWithLabel label="이메일" name="email" placeholder="이메일"/>
-                <InputWithLabel label="비밀번호" name="password" placeholder="비밀번호" type="password"/>
+                <InputWithLabel 
+                    label="이메일" 
+                    name="email" 
+                    placeholder="이메일" 
+                    value={email} 
+                    onChange={handleChange}
+                />
+                <InputWithLabel 
+                    label="비밀번호" 
+                    name="password" 
+                    placeholder="비밀번호" 
+                    type="password" 
+                    value={password} 
+                    onChange={handleChange}
+                />
                 <AuthButton>로그인</AuthButton>
                 <RightAlignedLink to="/auth/register">회원가입</RightAlignedLink>
             </AuthContent>
@@ -14,4 +45,11 @@ class Login extends Component {
     }
 }
 
-export default Login;
+export default connect(
+    (state) => ({
+        form: state.auth.getIn(['login', 'form'])
+    }),
+    (dispatch) => ({
+        AuthActions: bindActionCreators(authActions, dispatch)
+    })
+)(Login);
