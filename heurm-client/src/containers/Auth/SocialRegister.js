92304bf217c7406c134ad278779bb8bc90fc26b3
velopert <public.velopert@gmail.com>
2017-07-02 02:32:08 +0900

    소셜 회원가입 연동

--- heurm-client/src/containers/Auth/SocialRegister.js
+++ heurm-client/src/containers/Auth/SocialRegister.js
@@ -4,6 +4,9 @@ import { connect } from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as authActions from 'redux/modules/auth';
 import * as userActions from 'redux/modules/user';
+import {isLength, isAlphanumeric} from 'validator';
+import debounce from 'lodash/debounce';
+import storage from 'lib/storage';
 
 class SocialRegister extends Component {
 
@@ -16,11 +19,45 @@ class SocialRegister extends Component {
             value,
             form: 'socialRegister'
         });
+
+        if(!this.validateUsername(value)) return false; // 검증실패하면 여기서 멈춤
+        this.checkUsernameExists(value); // 검증은 성공했다면 중복 아이디 확인 시도
+    }
+
+    validateUsername = (value) => {
+        if(!isAlphanumeric(value) || !isLength(value, { min: 4, max: 15 })) {
+            this.setError('아이디는 4~15 글자의 알파벳/숫자로 이뤄져있어야합니다.');
+            return false;
+        }
+        return true;
+    }
+
+    checkUsernameExists = debounce(async (username) => {
+        if(username === '') return this.setError(null);
+        const { AuthActions } = this.props;
+        try {
+            await AuthActions.checkUsernameExists(username);
+            if(this.props.exists) {
+                this.setError('이미 존재하는 아이디입니다.'); 
+            } else {
+                this.setError(null);
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }, 300)
+
+
+    componentWillMount() {
+        const { social, history } = this.props;
+        if(!social.get('accessToken')) {
+            history.push('/auth/login');
+        }
     }
 
     componentWillUnmount() {
         const { AuthActions } = this.props;
-        AuthActions.initializeForm('socialRegister')
+        AuthActions.initializeForm('socialRegister');
     }
 
     setError = (message) => {
@@ -33,8 +70,30 @@ class SocialRegister extends Component {
     }
 
 
-    handleSocialRegister = () => {
+    handleSocialRegister = async () => {
+        const { AuthActions, UserActions, error, form, social, history } = this.props;
+        const username = form.get('username');
 
+        // 에러가 나있으면 멈춤
+        if(error) return;
+
+        const { accessToken, provider } = social.toJS();
+
+        try {
+            await AuthActions.socialRegister({
+                provider,
+                accessToken,
+                username
+            });
+        } catch (e) {
+            console.log(e);
+            return;
+        }
+
+        const loggedInfo = this.props.result.toJS();
+        UserActions.setLoggedInfo(loggedInfo);
+        history.push('/');
+        storage.set('loggedInfo', loggedInfo);
     }
 
     render() {
@@ -65,7 +124,9 @@ export default connect(
     (state) => ({
         form: state.auth.getIn(['socialRegister', 'form']),
         error: state.auth.getIn(['socialRegister', 'error']),
-        result: state.auth.get('result')
+        result: state.auth.get('result'),
+        social: state.auth.get('social'),
+        exists: state.auth.getIn(['register', 'exists', 'username']),
     }),
     (dispatch) => ({
         AuthActions: bindActionCreators(authActions, dispatch),
