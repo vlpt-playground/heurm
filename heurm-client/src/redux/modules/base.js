889c6eb9b0484582032c18929ab1ddae5cb77b1b
velopert <public.velopert@gmail.com>
2017-07-02 02:44:40 +0900

    base 리덕스 모듈에서 유저메뉴 관련 액션 생성

--- heurm-client/src/redux/modules/base.js
+++ heurm-client/src/redux/modules/base.js
@@ -2,15 +2,21 @@ import { Map } from 'immutable';
 import { handleActions, createAction } from 'redux-actions';
 
 const SET_HEADER_VISIBILITY = 'base/SET_HEADER_VISIBILITY'; // 헤더 렌더링 여부 설정
+const SET_USER_MENU_VISIBILITY = 'base/SET_USER_MENU_VISIBILITY' // 유저 메뉴 렌더링 여부 설정
 
 export const setHeaderVisibility = createAction(SET_HEADER_VISIBILITY); // visible
+export const setUserMenuVisibility = createAction(SET_USER_MENU_VISIBILITY); // visible
 
 const initialState = Map({
     header: Map({
         visible: true
+    }),
+    userMenu: Map({
+        visible: false
     })
 });
 
 export default handleActions({
-    [SET_HEADER_VISIBILITY]: (state, action) => state.setIn(['header', 'visible'], action.payload)
+    [SET_HEADER_VISIBILITY]: (state, action) => state.setIn(['header', 'visible'], action.payload),
+    [SET_USER_MENU_VISIBILITY]: (state, action) => state.setIn(['userMenu', 'visible'], action.payload)
 }, initialState);
