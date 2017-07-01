efdc7df869135d8f9aa679732b6432f1209c4c5b
velopert <public.velopert@gmail.com>
2017-07-02 02:45:39 +0900

    헤더 컨테이너 썸네일 클릭시 유저메뉴 렌더링

--- heurm-client/src/containers/Base/HeaderContainer.js
+++ heurm-client/src/containers/Base/HeaderContainer.js
@@ -2,25 +2,33 @@ import React, { Component } from 'react';
 import Header, { LoginButton, UserThumbnail } from 'components/Base/Header';
 import { connect } from 'react-redux';
 import * as userActions from 'redux/modules/user';
+import * as baseActions from 'redux/modules/base';
 import { bindActionCreators } from 'redux';
-import storage from 'lib/storage';
+import UserMenuContainer from './UserMenuContainer';
 
 
 
 class HeaderContainer extends Component {
 
+    handleThumbnailClick = () => {
+        const { BaseActions } = this.props;
+        BaseActions.setUserMenuVisibility(true);
+    }
+
     render() {
         const { visible, user } = this.props;
-        if(!visible) return null;
+        const { handleThumbnailClick } = this;
 
+        if(!visible) return null;
 
 
         return (
             <Header>
                 { user.get('logged') 
-                    ? (<UserThumbnail thumbnail={user.getIn(['loggedInfo', 'thumbnail'])}/>)
+                    ? (<UserThumbnail thumbnail={user.getIn(['loggedInfo', 'thumbnail'])} onClick={handleThumbnailClick}/>)
                     : <LoginButton/> 
                 }
+                <UserMenuContainer eventTypes="click"/>
             </Header>
         );
     }
@@ -32,6 +40,7 @@ export default connect(
         user: state.user
     }),
     (dispatch) => ({
-        UserActions: bindActionCreators(userActions, dispatch)
+        UserActions: bindActionCreators(userActions, dispatch),
+        BaseActions: bindActionCreators(baseActions, dispatch)
     })
 )(HeaderContainer);
