257db9e3200b4d1225e8678745efad84d0513b1b
velopert <public.velopert@gmail.com>
2017-07-03 00:50:02 +0900

    WRITE_POST 액션

--- heurm-client/src/redux/modules/home.js
+++ heurm-client/src/redux/modules/home.js
@@ -2,11 +2,16 @@ import { createAction, handleActions } from 'redux-actions';
 
 import { Map } from 'immutable';
 
+import { pender } from 'redux-pender';
+import * as PostsAPI from 'lib/api/posts';
+
 // action types
 const CHANGE_WRITE_POST_INPUT = 'home/CHANGE_WRITE_POST_INPUT';
+const WRITE_POST = 'home/WRITE_POST';
 
 // action creator
 export const changeWritePostInput = createAction(CHANGE_WRITE_POST_INPUT);
+export const writePost = createAction(WRITE_POST, PostsAPI.write);
 
 // initial state
 const initialState = Map({
@@ -17,5 +22,9 @@ const initialState = Map({
 
 // reducer
 export default handleActions({
-    [CHANGE_WRITE_POST_INPUT]: (state, action) => state.setIn(['writePost', 'value'], action.payload)
+    [CHANGE_WRITE_POST_INPUT]: (state, action) => state.setIn(['writePost', 'value'], action.payload),
+    ...pender({
+        type: WRITE_POST,
+        onPending: (state, action) => state.setIn(['writePost', ''])
+    })
 }, initialState);
