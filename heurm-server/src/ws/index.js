6b15a0c45edc7eecab4ef8f7d253a306e5e0615e
velopert <public.velopert@gmail.com>
2017-07-03 05:28:05 +0900

    socket 코드 수정

--- heurm-server/src/ws/index.js
+++ heurm-server/src/ws/index.js
@@ -3,13 +3,22 @@ const Router = require('koa-router');
 const ws = new Router();
 const dispatcher = require('lib/dispatcher');
 
+const connections = [];
+
+dispatcher.on('NEW_POST', post => {
+    connections.forEach(socket => {
+        socket.send(JSON.stringify(post));
+    });
+});
+
 ws.get('/ws', (ctx, next) => {
-    // ctx.websocket.send('Hello World');
-    // this.websocket.on('message', function(message) {
-    //     console.log(message);
-    // });
-    dispatcher.on('NEW_POST', post => {
-        ctx.websocket.send(JSON.stringify(post));
+    connections.push(ctx.websocket);
+    const index = connections.length - 1;
+    console.log('Client #' + index + ' has connected');
+
+    ctx.websocket.on('close', () => {
+        connections.splice(index, 1);
+        console.log('Client #' + index + ' has disconnected');
     });
 });
 
