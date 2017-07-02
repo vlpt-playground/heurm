02a947a12d65fe2017b68f896bf5ce488cff1bee
velopert <public.velopert@gmail.com>
2017-07-03 00:03:19 +0900

    포스트 작성 API

--- heurm-server/src/api/posts/posts.controller.js
+++ heurm-server/src/api/posts/posts.controller.js
@@ -1,5 +1,54 @@
+const Account = require('models/Account');
+const Post = require('models/Post');
+const Joi = require('joi');
+
 exports.write = async (ctx) => {
-    ctx.body = 'write';
+    const { user } = ctx.request;
+
+    if(!user) {
+        // 비로그인 에러
+        ctx.status = 403;
+        ctx.body = { message: ' not logged in' };
+        return;
+    }
+
+    let account;
+    try {
+        account = await Account.findById(user._id).exec();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+    
+    const count = account.thoughtCount;
+
+    // 스키마 검증하기
+    const schema = Joi.object().keys({
+        content: Joi.string().min(5).max(1000).required() // 5~1000 자
+    });
+
+    const result = Joi.validate(ctx.request.body, schema);
+
+    if(result.error) {
+        // 스키마 오류 발생
+        ctx.status = 400; // Bad request
+        return;
+    }
+
+    const { content } = ctx.request.body;
+
+    let post;
+    try {
+        post = await Post.write({
+            count,
+            username: user.profile.username,
+            content
+        });
+        await account.increaseThoughtCount();
+    } catch (e) {
+        ctx.throw(500, e);
+    }
+
+    ctx.body = post;
 };
 
 exports.list = async (ctx) => {
