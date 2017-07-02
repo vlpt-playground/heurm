4e1c54cedf2326055e175b21e6b97912bbf839c2
velopert <public.velopert@gmail.com>
2017-07-02 19:13:45 +0900

    WritePost 컴포넌트 틀 구성

---
+++ heurm-client/src/containers/Home/WritePostContainer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import WritePost from 'components/Home/WritePost';
+
+class WritePostContainer extends Component {
+    render() {
+        return (
+            <WritePost>
+                
+            </WritePost>
+        );
+    }
+}
+
+export default WritePostContainer;
--- heurm-client/src/pages/Home.js
+++ heurm-client/src/pages/Home.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { PageWrapper } from 'components/Base';
+import { WritePostContainer } from 'containers/Home';
 
 class Home extends Component {
     render() {
         return (
             <PageWrapper responsive>
-                Home
+                <WritePostContainer/>
             </PageWrapper>
         );
     }
