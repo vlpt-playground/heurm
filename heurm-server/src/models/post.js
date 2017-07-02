831d11596da5f98af040da36b744b1993a489800
velopert <public.velopert@gmail.com>
2017-07-02 23:14:08 +0900

    불필요 코드 제거

--- heurm-server/src/models/post.js
+++ heurm-server/src/models/post.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
-const { ObjectId } = mongoose.Types;
 
 const Comment = new Schema({
     createdAt: { type: Date, default: Date.now },
