1e95129d5961fbfb932aeaa0275b65d01b983205
velopert <public.velopert@gmail.com>
2017-07-02 23:25:01 +0900

    api 인덱스에서 posts 사용

--- heurm-server/src/api/index.js
+++ heurm-server/src/api/index.js
@@ -2,7 +2,8 @@ const Router = require('koa-router');
 
 const api = new Router();
 const auth = require('./auth');
+const posts = require('./posts');
 
 api.use('/auth', auth.routes());
-
+api.use('/posts', posts.routes());
 module.exports = api;
