aa48242a19360c353cb449104746ec7e85b2aac6
velopert <public.velopert@gmail.com>
2017-07-02 23:13:43 +0900

    Post 모델 생성

---
+++ heurm-server/src/models/post.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const { ObjectId } = mongoose.Types;
+
+const Comment = new Schema({
+    createdAt: { type: Date, default: Date.now },
+    username: String, 
+    _user: Schema.Types.ObjectId,
+    text: String
+});
+
+const Post = new Schema({
+    createdAt: { type: Date, default: Date.now },
+    count: Number,
+    username: String,
+    content: String,
+    likesCount: { type: Number, default: 0 },
+    likes: { type: [String], default: [] },
+    comments: { 
+        type: [Comment],
+        default: []
+    }
+});
+
+module.exports = mongoose.model('Post', Post);
