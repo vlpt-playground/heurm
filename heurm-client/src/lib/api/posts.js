7c3c3c1e10e1588116adc8259d34bfb9dcfeb33b
velopert <public.velopert@gmail.com>
2017-07-04 04:35:21 +0900

    대괄호 제거

--- heurm-client/src/lib/api/posts.js
+++ heurm-client/src/lib/api/posts.js
@@ -5,5 +5,5 @@ export const list = () => axios.get('/api/posts');
 export const listOfUser = (username) => axios.get(`/api/posts?username=${username}`);
 export const next = (url) => axios.get(url);
 
-export const like = (postId) => {axios.post(`/api/posts/${postId}/likes`);}
+export const like = (postId) => axios.post(`/api/posts/${postId}/likes`);
 export const unlike = (postId) => axios.delete(`/api/posts/${postId}/likes`);
