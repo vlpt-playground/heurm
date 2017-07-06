3842927ff584fa0abd90fe70b6765aa6f35e9715
velopert <public.velopert@gmail.com>
2017-07-07 00:16:37 +0900

    UserThumbnail 만들기

--- heurm-client/src/containers/Base/HeaderContainer.js
+++ heurm-client/src/containers/Base/HeaderContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Header, { LoginButton } from 'components/Base/Header';
+import Header, { LoginButton, UserThumbnail } from 'components/Base/Header';
 import { connect } from 'react-redux';
 import * as userActions from 'redux/modules/user';
 import { bindActionCreators } from 'redux';
@@ -30,9 +30,9 @@ class HeaderContainer extends Component {
         return (
             <Header>
                 { user.get('logged') 
-                    ? (<div>
-                        {user.getIn(['loggedInfo', 'username'])} <div onClick={this.handleLogout}>(로그아웃)</div>
-                    </div> )
+                    ? (
+                        <UserThumbnail thumbnail={user.getIn(['loggedInfo', 'thumbnail'])}/>
+                    )
                     : <LoginButton/> 
                 }
             </Header>
