f636ad199a8442f433fd2efe9be72471103545ba
velopert <public.velopert@gmail.com>
2017-07-08 04:35:16 +0900

    초기로딩 API 호출하기

--- heurm-client/src/containers/Shared/PostList/PostListContainer.js
+++ heurm-client/src/containers/Shared/PostList/PostListContainer.js
@@ -1,7 +1,23 @@
 import React, { Component } from 'react';
 import PostList from 'components/Shared/PostList';
+import { connect } from 'react-redux';
+import {bindActionCreators} from 'redux';
+import * as postsActions from 'redux/modules/posts';
 
 class PostListContainer extends Component {
+
+    load = async () => {
+        // 가장 최근 작성된 포스트 20개를 불러옵니다.
+        const { PostsActions } = this.props;
+        PostsActions.loadPost();
+    }
+
+    componentDidMount() {
+        // 컴포넌트가 마운트 됐을 때 호출 합니다.
+        this.load();
+    }
+    
+
     render() {
         return (
             <PostList/>
@@ -9,4 +25,12 @@ class PostListContainer extends Component {
     }
 }
 
-export default PostListContainer;
+export default connect(
+    (state) => ({
+        next: state.posts.get('next'),
+        data: state.posts.get('data')
+    }),
+    (dispatch) => ({
+        PostsActions: bindActionCreators(postsActions, dispatch)
+    })
+)(PostListContainer);
