b15dec3a538dc23c281ae30d25d0d857d722d65c
velopert <public.velopert@gmail.com>
2017-07-07 22:26:03 +0900

    list 메소드에서 초기로딩 로직 작성하기

--- heurm-server/src/models/post.js
+++ heurm-server/src/models/post.js
@@ -28,4 +28,17 @@ Post.statics.write = function({count, username, content}) {
     return post.save();
 };
 
+// 포스트 리스팅
+Post.statics.list = function({cursor, username, self}) {
+    // 파라미터에 cursor, username, self 가 담긴 객체를 받게 되는데, 
+    // 이번 구현 할 때에는 사용되지 않습니다.
+
+    // 나중에 파라미터에 받는 값에 따라 다른 쿼리내용을 가지게 됩니다. 일단은 특별 조건 없이 일반 적인 쿼리를 먼저 구현하겠습니다.
+    const query = {}; 
+
+    return this.find(query)
+        .sort({_id: -1}) // _id 역순
+        .limit(20) // 20개로 제한
+        .exec();
+};
 module.exports = mongoose.model('Post', Post);
