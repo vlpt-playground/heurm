78155a6ea27f5517ec7d389c94465155bac92049
velopert <public.velopert@gmail.com>
2017-07-08 00:08:50 +0900

    9-3. WritePost 에 TextArea 띄우기

--- heurm-client/src/components/Home/WritePost/WritePost.js
+++ heurm-client/src/components/Home/WritePost/WritePost.js
@@ -2,6 +2,8 @@ import React from 'react';
 import styled from 'styled-components';
 import oc from 'open-color';
 import { shadow } from 'lib/styleUtils';
+import Textarea from 'react-textarea-autosize';
+
 
 const Wrapper = styled.div`
     width: 768px;
@@ -11,9 +13,29 @@ const Wrapper = styled.div`
     ${shadow(1)}
 `;
 
-const WritePost = ({children}) => (
-    <Wrapper>
+const StyledTextarea = styled(Textarea)`
+    width: 100%;
+    background: transparent;
+    border: none;
+    resize: none;
+    outline: none;
+    font-size: 1.5rem;
+    font-weight: 300;
+    color: white;
+    ::placeholder {
+        color: ${oc.gray[3]};
+    }
+`;
 
+const WritePost = ({onChange, value}) => (
+    <Wrapper>
+        <StyledTextarea
+            minRows={3} 
+            maxRows={10} 
+            placeholder={`의식의 흐름대로 당신의 생각을 적어보세요.\n5초이상 아무것도 입력하지 않으면 자동으로 포스팅됩니다.`}
+            value={value}
+            onChange={onChange}
+        />
     </Wrapper>
 );
 
