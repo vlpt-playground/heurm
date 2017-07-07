73041d8fdca80e10504f42545fb1c21332f97e47
velopert <public.velopert@gmail.com>
2017-07-07 09:57:42 +0900

    포스트 관련 API 틀 준비하기

--- heurm-server/src/api/index.js
+++ heurm-server/src/api/index.js
@@ -2,7 +2,9 @@ const Router = require('koa-router');
 
 const api = new Router();
 const auth = require('./auth');
+const posts = require('./posts');
 
 api.use('/auth', auth.routes());
+api.use('/posts', posts.routes());
 
 module.exports = api;
